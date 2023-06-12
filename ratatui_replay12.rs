d930fb3ebcb27c2b3a8cd49311d4f135b083fb55
XizheYin_nju <62123683+XizheYin-NJU@users.noreply.github.com>
2023-06-13 01:58:37 +0800

    Create ratatui_replay12.rs

---
+++ ratatui_replay12.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let _local2 = ratatui::layout::Rect::new(65407, 65384, 26728, 65384);
+    let _local9 = ratatui::layout::Rect::new(26728, 26728, 26728, 26728);
+    let _local10 = ratatui::layout::Rect::union(_local2, _local9);
+}
