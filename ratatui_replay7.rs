9783a7d66d26ba8a37b6aec2833798882635d894
XizheYin_nju <62123683+XizheYin-NJU@users.noreply.github.com>
2023-06-13 01:55:02 +0800

    Create ratatui_replay7.rs

---
+++ ratatui_replay7.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let _local3 = ratatui::layout::Rect::new(11051, 128, 11009, 0);
+    let _local4 = ratatui::layout::Rect::new(11051, 11051, 65407, 0);
+    let _ = ratatui::layout::Rect::intersects(_local3, _local4);
+}
